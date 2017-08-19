49ac54e13cad5a0e1c94c2e3c1bdc0e854c4f6a0
Piotr <piotr00it@gmail.com>
2017-08-19 14:56:00 +0200

    szyfrowanie

--- web/js/common.js
+++ web/js/common.js
@@ -28,16 +28,75 @@ $(document).ready(function() {
  * AuiEditor init
  */
 function AuiEditorInit(id) {
+    $("#"+id)
+        .before("<textarea id='"+id+"Textarea' hidden></textarea>")
+        .before("<textarea id='"+id+"Annotations' hidden></textarea>")
+    ;
+
     YUI().use(
         'aui-ace-editor',
         function (Y) {
-            new Y.AceEditor(
+            var editor = new Y.AceEditor(
                 {
                     boundingBox: '#'+id,
                     mode: 'javascript',
                     width: '100%',
                 }
+            );
+            editor.render();
+
+            editor.getSession().on('change', function () {
+
+                var annotations = editor.getSession().getAnnotations();
+
+                var str = "";
+                for (var key in annotations) {
+                    if (annotations.hasOwnProperty(key))
+                        str += annotations[key].text + "on line " + " " + (annotations[key].row + 1) + "<br><br>";
+                }
+
+                $('#'+id+'Annotations').val(str);
+
+                $('#'+id+'Textarea').val(editor.getSession().getValue());
+            });
+        }
+    );
+}
+
+/**
+ * AuiModal init
+ */
+function AuiModalInit(body,header,id)
+{
+    YUI().use(
+        'aui-modal',
+        function(Y) {
+            var modal = new Y.Modal(
+                {
+                    bodyContent: body,
+                    centered: true,
+                    headerContent: header,
+                    modal: true,
+                    render: '#'+id,
+                    width: 650
+                }
             ).render();
         }
     );
+}
+
+/**
+ * Alert init
+ */
+function createAlert(body,header)
+{
+    // console.log($('#alert'));
+
+    if ( $( "#alert" ).length === 0 )
+    {
+        $('body').append('<div class="yui3-skin-sam"><div id="alert"></div></div>');
+    }
+
+
+    AuiModalInit(body,header,'alert')
 }
