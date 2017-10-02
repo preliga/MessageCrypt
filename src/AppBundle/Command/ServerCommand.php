8d5dfcbae4edcd2d761ffcdfd1d9c0c44d4c8ec7
Piotr Religa <preliga@confido.pl>
2017-10-02 17:05:36 +0200

    web servis

--- src/AppBundle/Command/ServerCommand.php
+++ src/AppBundle/Command/ServerCommand.php
@@ -17,6 +17,9 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use React\EventLoop\Factory as LoopFactory;
+use React\Socket\Server as Reactor;
+
 class ServerCommand extends ContainerAwareCommand
 {
 
@@ -32,15 +35,32 @@ class ServerCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $loop = LoopFactory::create();
+        $socket = new Reactor('127.0.0.1:8080', $loop);
 
-        $server = IoServer::factory(new HttpServer(
-            new WsServer(
-                new Notification($this->getContainer())
-            )
-        ), 8080);
+//        $server = IoServer::factory(new HttpServer(
+//            new WsServer(
+//                new Notification($loop)
+//            )
+//        ), 8080);
+        $server = new IoServer(new HttpServer(new WsServer(new Notification($loop))), $socket, $loop);
 
+//        $server->loop->nextTick(function(){
+//
+//        });
+
+//        $conn = $this->getContainer();
+
+//        for($i = 0; $i < 100; $i++)
+//        {
+//            $conn->send($i);
+//            sleep(1);
+//        }
+//
         $server->run();
 
     }
+
+//    public function tick
 
 }
